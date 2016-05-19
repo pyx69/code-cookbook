8b7bd685811d8a3c7d15a277f00b310c53c860a8
Mark Knol <mark.knol@gmail.com>
2016-05-19 11:31:05 +0200

    cppia test

---
+++ build-site-CPPIA.bat
@@ -0,0 +1,6 @@
+@echo off
+call lessc assets/includes/css/haxe-nav.css assets/includes/css/haxe-nav.min.css --clean-css="--s1 --advanced"
+call lessc assets/includes/css/styles.css assets/includes/css/styles.min.css --clean-css="--s1 --advanced"
+rmdir "output" /S /Q
+mkdir "output"
+haxelib run hxcpp CodeCookBook.cppia
